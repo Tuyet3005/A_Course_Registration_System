97af82ebd9a85a37408e0e371fa18ba998e35dd4
Icexy Kim <kimicexy2002@gmail.com>
2021-05-14 17:06:56 +0700

    Menu9+ File txt tai khoan sv, gv

--- QLSV/main.cpp
+++ QLSV/main.cpp
@@ -1,68 +1,23 @@
-#include<iostream>
-#include<string>
-#include<fstream>
-using namespace std;
-void Split(string s, string& tk, string& mk) //oke
-{
-	bool flag = true;
-	char k;
-	for (int i = 0; i < s.length(); i++)
-	{
-		if (s[i] == '.') flag = false;
-		else if (flag) tk += s[i];
-		else mk += s[i];
-	}
-}
-bool checkAccount(string tk, string mk, short lc)
-{
-	ifstream f;
-	if (lc == 1) f.open("HocSinh.txt");
-	else f.open("GiaoVu.txt");
-	string acc, pass, s;
-	while (!f.eof())
-	{
-		acc = "";
-		pass = "";
-		s = "";
-		f >> s;
-		Split(s, acc, pass);
-		if (acc != tk)
-		{
-			continue;
-		}
-		if (pass == mk) return true;
-	}
-	f.close();
-	return false;
-}
-bool LogIn(string& tk, string& mk, short& lc)
-{
-	cout << "--------------------DANG NHAP------------------------" << endl;
-	cout << "Nhap 1 neu ban la hoc sinh, 0 neu ban la giao vu" << endl;
-	cout << "Moi nhap: ";
-	cin >> lc;
-	cin.ignore();
-	char dem = 0;
-	while (dem < 5)
-	{
-		cout << "Tai khoan: ";
-		cin.clear();
-		getline(cin, tk, '\n');
-		cout << "Mat khau: ";
-		cin.clear();
-		getline(cin, mk, '\n');
-		if (checkAccount(tk, mk, lc)) return true;
-		else if (dem < 4) cout << "Ban da nhap sai... Moi nhap lai" << endl;
-		dem++;
-	}
-	cout << "Nhap sai qua 5 lan, ban bi buoc thoat !!!" << endl;
-	return false;
-}
+#include"variable.h"
+#include"login.h"
+#include"staff.h"
+#include"student.h"
 int main()
 {
 	string tk, mk;
-	short lc;
-	if (LogIn(tk, mk, lc)) cout << "DANG NHAP THANH CONG" << endl;
-	else cout << "DANG NHAP THAT BAI" << endl;
-	return 0;
+	short lc = 1;//0: giao vu ; 1: sinh vien
+	do
+	{
+		LogIn(tk, mk, lc);//dn sai thi tu dong thoat ra luon
+		system("cls");
+		if (lc == 0)
+		{
+			GiaoVu();
+		}
+		else if (lc == 1)
+		{
+			SinhVien();
+		}
+	} 	while (true);
+	return 0;	
 }
--- QLSV/variable.h
+++ QLSV/variable.h
@@ -1,23 +1,39 @@
 #pragma once
 #include<iostream>
 #include<string>
+#include<fstream>
 using namespace std;
+//THOI GIAN
+struct time
+{
+	int gio;
+	int phut;
+};
 struct Ngay
 {
 	int d;
 	int m;
 	int y;
 };
-//thoi gian cua hoc ky
 struct ThoiGian
 {
 	Ngay ngay_bd;
 	Ngay ngay_kt;
 };
-struct time
+//HOC KY
+struct BuoiHoc
 {
-	int gio;
-	int phut;
+	string thu;//mon, tue, wed,...
+	string buoi;//buoi(S1, S2, S3, S4)
+	//S1 (07:30), S2(09:30), S3(13:30), S4(15:30)
+};
+struct MonHoc
+{
+	string id;
+	string tenMon, tenGv;
+	int so_tc;
+	int MaxSv = 50;//sl sv toi da 
+	BuoiHoc bh1, bh2;//hai buoi trong tuan
 };
 struct NodeMon
 {
@@ -34,12 +50,7 @@ struct HocKy
 	ListMon dsMon;
 	ThoiGian tg;//thoi gian bd, kt
 };
-struct BuoiHoc
-{
-	string thu;//mon, tue, wed,...
-	string buoi;//buoi(S1, S2, S3, S4)
-	//S1 (07:30), S2(09:30), S3(13:30), S4(15:30)
-};
+//LOP HOC
 struct Sv
 {
 	int stt;
@@ -72,14 +83,7 @@ struct ListLopHoc
 	NodeLop* pHead;
 	NodeLop* pTail;
 };
-struct MonHoc
-{
-	string id;
-	string tenMon, tenGv;
-	int so_tc;
-	int MaxSv = 50;//sl sv toi da 
-	BuoiHoc bh1, bh2;//hai buoi trong tuan
-};
+//NAM HOC
 struct NamHoc
 {
 	ThoiGian tg;//chi cout nam (vd: tg.ngay_bd.y)
