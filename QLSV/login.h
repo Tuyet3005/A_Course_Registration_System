8cf17cb5090d75b2e00a21037f5fe398b9e498af
KimIcexy <84014907+KimIcexy@users.noreply.github.com>
2021-05-16 00:07:24 +0700

    Delete login.h

--- QLSV/login.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include"variable.h"
-bool checkAccount(string tk, string mk, short lc);
-void LogIn(string& tk, string& mk, short& lc);
-void changePass(short lc, string tk, string& mk);
