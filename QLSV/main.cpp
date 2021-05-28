6983abcafb05494a1b90268662c8ce7092986b50
BichLeee <bich1042002@gmail.com>
2021-05-28 23:56:42 +0700

    ff

--- QLSV/main.cpp
+++ QLSV/main.cpp
@@ -1,21 +1,28 @@
 #include"startMenu.h"
+#include"dkmh.cpp"
 int main()
 {
-	string tk, mk;
-	short lc = 1;//Ma tk:  0: giao vu ; 1: sinh vien
-	ListNamHoc listNam;
-	listNam.pHead = listNam.pTail = NULL;
-	//tai du lieu list nam hoc tu file len he thong
-	TaiNamTuFile(listNam);
-	do
-	{
-		bool Thoat = false;
-		LogIn(tk, mk, lc);//dn sai thi tu dong thoat ra luon
-		system("cls");
-		do
-		{
-			Thoat = XlMenuBD(LuaChon(InMenuBatDau(lc)), lc, tk, mk, listNam);
-		} 		while (!Thoat);
-		//
-	} 	while (true);
+	//string tk, mk;
+	//short lc = 1;//Ma tk:  0: giao vu ; 1: sinh vien
+	//ListNamHoc listNam;
+	//listNam.pHead = listNam.pTail = NULL;
+	////tai du lieu list nam hoc tu file len he thong
+	//TaiNamTuFile(listNam);
+	//do
+	//{
+	//	bool Thoat = false;
+	//	LogIn(tk, mk, lc);//dn sai thi tu dong thoat ra luon
+	//	system("cls");
+	//	do
+	//	{
+	//		Thoat = XlMenuBD(LuaChon(InMenuBatDau(lc)), lc, tk, mk, listNam);
+	//	} 		while (!Thoat);
+	//	//
+	//} 	while (true);
+	Time t;
+	t.h = 0;
+	t.m = 0;
+	t.s = 10;
+	Timer(t, 2, 10);
+	return 0;
 }
