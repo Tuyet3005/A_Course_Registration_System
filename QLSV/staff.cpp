8c24e919d015dd1931838bb3c6442c656818ce89
TUYẾT NGUYỄN THỊ ÁNH <20120422@student.hcmus.edu.vn>
2021-06-07 08:59:04 +0700

    1 su ngungok

--- QLSV/staff.cpp
+++ QLSV/staff.cpp
@@ -97,14 +97,14 @@ int InMenuTaoMoi(int chon)//dung cho ca menu Cap nhat
 	else
 	{
 		cout << "----MENU CAP NHAT----\n";
-		cout << "1. Nam hoc\n";
-		cout << "2. Lop hoc\n";
-		cout << "3. Hoc ky\n";
-		cout << "4. Mon hoc\n";
-		cout << "5. Quay ve\n";
-		cout << "6. Thoat\n";
-		return 6;
 	}
+	cout << "1. Nam hoc\n";
+	cout << "2. Lop hoc\n";
+	cout << "3. Hoc ky\n";
+	cout << "4. Mon hoc\n";
+	cout << "5. Quay ve\n";
+	cout << "6. Thoat\n";
+	return 6;
 }
 bool XlTaoMoi(int chon, ListNamHoc& l)
 {
