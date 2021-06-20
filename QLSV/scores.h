ec885ac7173810e8f43ed043839ed3abb3a26046
Tuyet3005 <hyxinhdep3005@gmail.com>
2021-06-20 23:43:11 +0700

    Đổi tên hàm findScore thành ChonDiem và thêm hàm NhapDiem để chọn điểm từ bàn phím

--- QLSV/scores.h
+++ QLSV/scores.h
@@ -1,8 +1,9 @@
 #pragma once
 #include"variable.h"
+#include"courses.h"
 
 
-NodeSv_Mon* findScore(NodeMon* mon, int mssv)
+NodeSv_Mon* ChonDiem(NodeMon* mon, int mssv)
 {
 	NodeSv_Mon* temp = mon->headSvMon;
 	while (temp != NULL)
@@ -14,4 +15,20 @@ NodeSv_Mon* findScore(NodeMon* mon, int mssv)
 		temp = temp->pNext;
 	}
 	return NULL;
+}
+
+
+NodeSv_Mon* NhapDiem(ListNamHoc& l)
+{
+	NodeMon* mon = NhapMonHoc(l);
+	NodeSv_Mon* diem;
+	int mssv;
+
+	do {
+		cout << "Nhap MSSV: ";
+		cin >> mssv;
+		diem = ChonDiem(mon, mssv);
+	} while (diem == NULL);
+
+	return diem;
 }
